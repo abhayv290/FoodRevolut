709d4cb196c6ab1321aa0168b25f3444e2049f91
abhay <abhayv290@gmail.com>
2026-04-30 21:30:58 +0530

    adding SSL/TLS with zerossl

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,18 +8,17 @@ DEBUG = False
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
-# ── Temporary (until HTTPS setup) ─────────────────────────────────────────────
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
 
-SECURE_HSTS_SECONDS = 0
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-SECURE_HSTS_PRELOAD = False
+# HTTPS security settings
+SECURE_PROXY_SSL_HEADER       = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT            = True
+SESSION_COOKIE_SECURE          = True
+CSRF_COOKIE_SECURE             = True
+SECURE_HSTS_SECONDS            = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD            = True 
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-USE_X_FORWARDED_HOST = True
- 
+
 # ── Static and media — both from S3 ──────────────────────────────────────────
 # Nginx no longer serves static or media files directly.
 # Browser fetches them from S3 URLs.
